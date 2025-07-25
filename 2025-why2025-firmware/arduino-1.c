10a54f411416ae883125f6cc77c63d205df077fa
Ayke van Laethem <aykevanlaethem@gmail.com>
2025-07-25 16:19:21 +0200

    WIP why2025

---
+++ 2025-why2025-firmware/arduino-1.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+// Memory mapped I/O (values from the datasheet).
+#define PORTB *(volatile uint8_t*)(0x23)
+#define DDRB  *(volatile uint8_t*)(0x24)
+
+int main() {
+    DDRB  = 1; // set pin PB0 as an output
+    PORTB = 1; // set pin PB4 high
+
+    // wait forever
+    while (1) {
+    }
+    return 0;
+}
